a4edf7b2f85bd0cee4d94a008b930b43dc6fbe23
DidlDimpfe <philwojczak@gmail.com>
2023-05-01 13:26:38 +0200

    Initial commit

---
+++ src/main/java/de/philw/textgenerator/TextGenerator.java
@@ -0,0 +1,17 @@
+package de.philw.textgenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TextGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
